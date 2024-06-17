d4869eced8f4757048199bb3919acbdc522ab602
Back67 <168211751+Back67@users.noreply.github.com>
2024-06-16 19:38:20 -0600

    Create LiteratulaApplication.java

---
+++ LiteratulaApplication.java
@@ -0,0 +1,12 @@
+package com.tu_usuario.literatula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteratulaApplication.class, args);
+    }
+}
